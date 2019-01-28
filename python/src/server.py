f94d9324405cb9d28a041fa254d7fd4e097e1aa0
Vincent <vincentp822@googlemail.com>
2019-01-28 21:13:23 +0100

    Hello World for Python with tests.

---
+++ python/src/server.py
@@ -0,0 +1,13 @@
+__author__ = 'Vincent Pietzsch'
+__date__ = '2019-01-28'
+__project__ = 'doc-service'
+
+
+class Server:
+
+    def __init__(self):
+        pass
+
+    def what_is_the_answer(self):
+        return 42
+
---
+++ python/tests/server_test.py
@@ -0,0 +1,12 @@
+import unittest
+from python.src.server import Server
+
+class ServerTest(unittest.TestCase):
+
+    def test_something(self):
+        s = Server()
+        self.assertEqual(42, s.what_is_the_answer())
+
+
+if __name__ == '__main__':
+    unittest.main()
